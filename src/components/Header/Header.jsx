5bac7441c93845e3a57230ea7ad065249bd8ab16
Leandro-Pinho <leandroessencia@gmail.com>
2023-08-05 18:52:13 -0300

    criando o submit do input

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,26 @@
 
-const Header = () => {
+const Header = (props) => {
+  const handleSubmit = (e) => {
+   // para não atualizar a pagina ao enviar o submit
+    e.preventDefault();
+
+    // para pegar o valor do input
+    const searchValue = e.target[0].value;
+
+    // pegando o ação submit
+    props.onSubmit(searchValue);
+
+    // para limpar o valor do input
+    e.target[0].value = ""; 
+    
+  }
+
   return (
     <div className="Header">
-      header
+      <h1>DNC Movie List</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="Pesquise por Filmes" />
+      </form>
     </div>
   )
 }
